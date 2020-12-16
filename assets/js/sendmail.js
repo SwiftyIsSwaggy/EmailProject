8644c31235cf6f6cebc0c784477c5f366b1d7837
SwiftyP <swiftyxswaggy@outlook.com>
2020-12-16 13:36:08 +0300

    Added a little 2 step verification

--- assets/js/sendmail.js
+++ assets/js/sendmail.js
@@ -1,20 +1,31 @@
 function sendEmail() { 
-    Email.send({ 
-      Host: "smtp.gmail.com", 
-      Username: document.getElementById("senderEm").value, 
-      Password: document.getElementById("senderPwd").value, 
-      To: document.getElementById("receverEm").value, 
-      From: document.getElementById("senderEm").value, 
-      Subject: "Email Sent using JS, Panguy", 
-      Body: document.getElementById("emailCont").value, 
-    }) 
-      .then(function (message) { 
-        alert("mail sent successfully") 
-        document.getElementById("senderEm").value="";
-        document.getElementById("senderPwd").value="";
-        document.getElementById("receverEm").value=""; 
-        document.getElementById("emailCont").value="";
-      }); 
+    var sender =document.getElementById("senderEm").value;
+    if (sender.includes("@gmail.com")){
+      Email.send({ 
+        Host: "smtp.gmail.com", 
+        Username: document.getElementById("senderEm").value, 
+        Password: document.getElementById("senderPwd").value, 
+        To: document.getElementById("receverEm").value, 
+        From: document.getElementById("senderEm").value, 
+        Subject: "Email Sent using JS, Panguy", 
+        Body: document.getElementById("emailCont").value, 
+      }) 
+        .then(function (message) { 
+          alert("mail sent successfully") 
+          document.getElementById("senderEm").value="";
+          document.getElementById("senderPwd").value="";
+          document.getElementById("receverEm").value=""; 
+          document.getElementById("emailCont").value="";
+        }); 
+    } 
+    else{
+      alert("Only @gmail.com accounts allowed")
+      document.getElementById("senderEm").value="";
+      document.getElementById("senderPwd").value="";
+      document.getElementById("receverEm").value=""; 
+      document.getElementById("emailCont").value="";
+    }
+
   } 
 function cancel(){
   document.getElementById("senderEm").value="";
