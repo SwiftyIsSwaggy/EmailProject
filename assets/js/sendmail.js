3e2a320cb15798328877728a5374040752403a1a
SwiftyP <swiftyxswaggy@outlook.com>
2020-12-16 13:23:28 +0300

    Fixed a few bugs

--- assets/js/sendmail.js
+++ assets/js/sendmail.js
@@ -1,14 +1,20 @@
 function sendEmail() { 
     Email.send({ 
       Host: "smtp.gmail.com", 
-      Username: document.getElementById("senderEm").innerText, 
-      Password: document.getElementById("senderPwd").innerText, 
-      To: document.getElementById("receverEm").innerText, 
-      From: document.getElementById("senderEm").innerText, 
+      Username: document.getElementById("senderEm").value, 
+      Password: document.getElementById("senderPwd").value, 
+      To: document.getElementById("receverEm").value, 
+      From: document.getElementById("senderEm").value, 
       Subject: "Email Sent using JS, Panguy", 
-      Body: document.getElementById("emailCont").textContent, 
+      Body: document.getElementById("emailCont").value, 
     }) 
       .then(function (message) { 
         alert("mail sent successfully") 
       }); 
   } 
+function cancel(){
+  document.getElementById("senderEm").value="";
+  document.getElementById("senderPwd").value="";
+  document.getElementById("receverEm").value=""; 
+  document.getElementById("emailCont").value="";
+}
